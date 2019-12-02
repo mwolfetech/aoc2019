b0dab8ea561a87cbab0f546800cf8d59533a7acf
Michael Wolfe <mwolfe3@hmc.psu.edu>
2019-12-01 22:45:25 -0500

    Initial Commit

---
+++ 1/the-tyranny-of-the-rocket-equation/test/the_tyranny_of_the_rocket_equation/core_test.clj
@@ -0,0 +1,7 @@
+(ns the-tyranny-of-the-rocket-equation.core-test
+  (:require [clojure.test :refer :all]
+            [the-tyranny-of-the-rocket-equation.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
