6afa5993cd85bd7ff692996f3f3f7c0e265445e1
Michael Wolfe <mwolfe3@hmc.psu.edu>
2019-12-21 12:23:59 -0500

    Day Six

---
+++ 6/universal-orbit-map/test/universal_orbit_map/core_test.clj
@@ -0,0 +1,7 @@
+(ns universal-orbit-map.core-test
+  (:require [clojure.test :refer :all]
+            [universal-orbit-map.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
